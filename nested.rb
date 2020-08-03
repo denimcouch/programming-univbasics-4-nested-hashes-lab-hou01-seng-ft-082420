f9600cb22378bf6f639f60039902332988c66c69
Alex Mata <alexmata92@ymail.com>
2020-08-03 19:10:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,5 +134,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-dennis_new_language = programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+dennis_new_language = programmer_hash[:dennis_ritchie][:languages] =
 end
