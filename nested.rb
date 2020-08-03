f39fd81cb6d9f8be04c4f0639a480c3f2cfa32aa
Alex Mata <alexmata92@ymail.com>
2020-08-03 19:01:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages.to_s]
-dennis_ritchies_language
+dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
+dennis_ritchies_language.to_s
 end
 
 def adding_matz
